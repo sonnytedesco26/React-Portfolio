286ebe792965fee0f1181906d48f51526f789815
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-05-21 15:31:57 -0400

    wrote up layout, trying to get navbar to work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import Entry from './components/Entry/Entry.js';
-
+import React, {useState}  from 'react';
+import Entry from './components/Entry/index.js';
+import Navbar from './components/Navbar/index.js';
+import Stuff from './components/Stuff/index.js';
+import Contact from './components/Contact/index.js';
 import './App.css';
 
 function App() {
+  const [spots] = useState([
+    {
+      name: 'work', description: 'some projects I have made for school in the past'
+    },
+    {
+      name: 'resume', description: 'Resume'
+    },
+    {
+      name: 'about me', description: 'about me'
+    },
+    {
+      name: 'contact me', description: 'feel free to email me here'
+    }
+  ]);
+
+  const [currentSpot, setCurrentSpot] = useState(spots[0]);
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div><Entry/></div>
+    <div>
+      <Navbar
+        spots = {spots}
+        setCurrentSpot = {setCurrentSpot}
+        currentSpot = {currentSpot}
+        contactSelected = {contactSelected}
+        setContactSelected = {setContactSelected}
+      ></Navbar>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Entry></Entry>
+            <Stuff> currentSpot={currentSpot}</Stuff>
+          </>
+        ) : (
+          <Contact></Contact>
+        )}
+      </main>
+    </div>
   );
 }
 
--- src/components/Entry/index.js
+++ src/components/Entry/index.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import './Entry.css';
-
 
 function Entry() {
     return (
